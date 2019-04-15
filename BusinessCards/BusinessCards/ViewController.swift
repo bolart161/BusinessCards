e1ca43a77d01d916b2f790d46e1b83cd5df4c342
Artem <artem@Artems-iMac.local>
2019-04-15 20:50:58 +0300

    Init Project

---
+++ BusinessCards/BusinessCards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BusinessCards
+//
+//  Created by Artem on 15/04/2019.
+//  Copyright © 2019 Artem Boltunov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
