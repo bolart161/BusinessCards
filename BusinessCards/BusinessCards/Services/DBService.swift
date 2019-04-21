c8db77471072a9a7d0a34eab778433755cefb917
Mordvintseva A <mordvintseva.ai@gmail.com>
2019-04-21 04:51:28 -0700

    Change get method
    Change get(field: String, value: String) method to get(field: String, value: Any)

--- BusinessCards/BusinessCards/Services/DBService.swift
+++ BusinessCards/BusinessCards/Services/DBService.swift
@@ -37,8 +37,10 @@ class DBService<T: Object>: DBServiceProtocol {
         }
     }
 
-    func get(field: String, value: String, sortBy: String? = nil, ascending: Bool = true) -> Results<T> {
-        let predicate = NSPredicate(format: "\(field) == [c]%@", value)
+    func get(field: String, value: Any, sortBy: String? = nil, ascending: Bool = true) -> Results<T> {
+        // swiftlint:disable force_cast
+        let predicate = NSPredicate(format: "\(field) == [c]%@", value as! CVarArg)
+        // swiftlint:enable force_cast
         if let sortField = sortBy {
             return realm.objects(T.self).filter(predicate).sorted(byKeyPath: sortField, ascending: ascending)
         }
